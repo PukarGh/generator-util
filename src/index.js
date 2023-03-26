85ac231df71093c2061e78eff5141fc20f7f227f
PukarGh <78738194+PukarGh@users.noreply.github.com>
2023-03-26 12:31:58 +0545

    Refactor

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const TEMPLATE_TO_EXTENSION_MAPPING = {
     'styles': 'module.css',
     'test': 'test.tsx'
 };
-
 const DEFAULT_CONFIG = {
     outputPath: 'src/components',
     templatePath: 'src/templates'
@@ -83,16 +82,16 @@ const replacePlaceholders = (template, mappings) => {
 
     const mappings = getMappings(componentName);
 
-    const directory = path.join(paths.outputPath, mappings['{COMPONENT_NAME}']);
+    const outputDirectory = path.join(paths.outputPath, mappings['{COMPONENT_NAME}']);
 
     console.log('\x1b[94m Creating Directory ... \x1b[0m');
-    if (fs.existsSync(directory)){
-        console.error('\x1b[91m ERROR: Write Directory already exists! \x1b[0m');
+    if (fs.existsSync(outputDirectory)){
+        console.error('\x1b[91m ERROR: Output Directory already exists! \x1b[0m');
         return 1;
     }
 
     try {
-        fs.mkdirSync(directory, {recursive: true});
+        fs.mkdirSync(outputDirectory, {recursive: true});
     } catch (e){
         console.error('\x1b[91m ERROR: Cannot create output directory! \x1b[0m');
         console.error(`\x1b[91m ${e.message}\x1b[0m`);
@@ -103,7 +102,7 @@ const replacePlaceholders = (template, mappings) => {
         console.log(`\x1b[94m Generating ${name} file ... \x1b[0m`);
 
         const output = replacePlaceholders(template, mappings);
-        const outFileName = path.join(directory, (name === 'index' ? 'index' : mappings['{COMPONENT_NAME}']) + '.' + TEMPLATE_TO_EXTENSION_MAPPING[name]);
+        const outFileName = path.join(outputDirectory, (name === 'index' ? 'index' : mappings['{COMPONENT_NAME}']) + '.' + TEMPLATE_TO_EXTENSION_MAPPING[name]);
 
         fs.writeFileSync(outFileName, output);
     });
